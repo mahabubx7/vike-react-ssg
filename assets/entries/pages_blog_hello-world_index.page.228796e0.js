48fbdb21df6ff17a7d76d17edd312010fa3cf0e6
Mahabub Alam <mahabubx7@gmail.com>
2023-10-29 01:12:39 +0600

    Updates

---
+++ assets/entries/pages_blog_hello-world_index.page.228796e0.js
@@ -0,0 +1,1 @@
+import{j as r}from"../chunks/chunk-4d04ce61.js";function e(){return r.jsxs(r.Fragment,{children:[r.jsx("h1",{children:"Hello World"}),r.jsx("p",{children:"A post from blog."})]})}export{e as Page};
